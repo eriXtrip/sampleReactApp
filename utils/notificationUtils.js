f763e975e12c276cabbe58b8b141a1abc5eac42e
Kristelle Anne <Kristelle Anne@LAPTOP-VUA4U40P>
2025-12-04 17:12:18 +0800

    add notif toast not done

--- utils/notificationUtils.js
+++ utils/notificationUtils.js
@@ -1,10 +1,20 @@
 // utils/notificationUtils.js
 
 import * as Notifications from 'expo-notifications';
+import { showToast } from 'react-native-nitro-toast';
+
+//   
 
 export const triggerLocalNotification = async (title, body) => {
-  await Notifications.scheduleNotificationAsync({
-    content: { title, body },
-    trigger: null,
+  showToast(body, {
+    title,
+    type: 'info',
+    position: 'top',
+    duration: 3500,
+    presentation: 'alert',
+    haptics: true,
+    backgroundColor: '#333333',
+    titleColor: '#ffffff',
+    messageColor: '#e0e0e0',
   });
 };
